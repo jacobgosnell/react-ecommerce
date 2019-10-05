166bdcb61a1cac3c2ad8c1feece3b8a7b914aa76
Jake <gosnelljacob@gmail.com>
2019-10-04 21:55:30 -0700

    setup reduxform

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { Router, Switch, Route } from "react-router-dom";
 import reducers from "./reducers";
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware()(compose((window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore)));
 
 import "./style/main.scss";
 
@@ -22,6 +22,8 @@ function main() {
         <Layout>
           <Switch>
             <Route path='/' exact component={SignIn}/>
+            <Route path='/' exact component={SignIn}/>
+            <Route path='/signup' exact component={SignUp}/>
           </Switch>
         </Layout>
       </Router>
--- src/components/auth/sign-in.js
+++ src/components/auth/sign-in.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 
+import { reduxForm, Field } from 'redux-form';
+
 class SignIn extends Component {
   render() {
     return (
       <div className='sign-in'>
         Sign in
       </div>
-    )
+    );
   }
 }
+
+SignIn = reduxForm({
+  form: 'Signin'
+})(SignIn);
 
 export default SignIn;
