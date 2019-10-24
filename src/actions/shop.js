e6749fb174e5211e3ac47b3ca0639cad2723400d
Jake <gosnelljacob@gmail.com>
2019-10-23 23:37:12 -0700

    developed shopreducer and fetch shop categories action creator

---
+++ src/actions/shop.js
@@ -0,0 +1,40 @@
+import {
+  // SET_SHOP_CATEGORIES
+  SET_NAVBAR_LINKS
+} from './types';
+
+export function fetchShopCategories() {
+  return ({
+    type: SET_NAVBAR_LINKS,
+    payload: [
+      {
+        _id: 0,
+        title: 'All'
+      },
+      {
+        _id: 1,
+        title: 'JavaScript'
+      },
+      {
+        _id: 2,
+        title: 'UI/UX'
+      },
+      {
+        _id: 3,
+        title: 'Python'
+      },
+      {
+        _id: 4,
+        title: 'UML'
+      },
+      {
+        _id: 5,
+        title: 'Ruby'
+      },
+      {
+        _id: 6,
+        title: 'Linux'
+      }
+    ]
+  })
+}
--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -14,6 +14,7 @@ class Shop extends Component {
       }
     ]
     this.props.setHeaderLinks(headerLinks);
+    this.props.fetchShopCategories();
     // fetch shop products action creator
     // set the header links
     // fetch navbar links
