5a1ac1940dca8c0af3a1b6e1c13dc8da2993c816
Jake <gosnelljacob@gmail.com>
2019-10-25 17:40:10 -0700

    fixed bug

--- src/components/shop/shopSearchBar.js
+++ src/components/shop/shopSearchBar.js
@@ -5,8 +5,8 @@ import {reduxForm, Field } from 'redux-form';
 function FormSearchBar(props) {
   return (
     <div className='search-bar-grid'>
-      <i className="fas fa-search-bar-grid__icon"></i>
-      <input className={`${props.className} form-search-bar`} {...props.input} type='text' placeholder={`${props.placeholder}`}/>
+      <i className="fas fa-search search-bar-grid__icon"></i>
+      <input className={`${props.className} form-search-bar search-bar-grid__input`} {...props.input} type='text' placeholder={`${props.placeholder}`}/>
     </div>
   );
 }
