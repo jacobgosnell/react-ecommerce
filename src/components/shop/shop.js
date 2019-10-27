34009fc2b259638c2d571982a732a5021ddbe3d6
Jake <gosnelljacob@gmail.com>
2019-10-26 20:55:58 -0700

    structured shop cart component and styled

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import * as actions from '../../actions';
 import ShopSearchBar from './shopSearchBar';
 import ShopProduct from './shopProduct';
+import ShopCart from './shopCart';
 
 class Shop extends Component {
 
@@ -48,7 +49,7 @@ class Shop extends Component {
             })
           }
         </div>
-        {/* shop cart button*/}
+        <ShopCart className='shop__cart'/>
       </div>
     )
   }
