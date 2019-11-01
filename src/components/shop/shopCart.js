19f9c109f45345d1da372112eedb6feaf2fe0d5c
Jake <gosnelljacob@gmail.com>
2019-10-31 18:51:52 -0700

    debugging shopCart component

--- src/components/shop/shopCart.js
+++ src/components/shop/shopCart.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-function CartButton({className, icon}) {
+function CartButton ({className, icon}) {
   return (
     <div className={`${className} cart-button`}>
       <i className={icon}/>
@@ -8,7 +8,7 @@ function CartButton({className, icon}) {
   )
 }
 
-function CartContent({ className, products }) {
+function CartContent ({ className, products }) {
   let count = products.length;
   let productsJSX = products.map(product => <h1 key={product}>{product}</h1>);
   return (
@@ -24,7 +24,7 @@ function CartContent({ className, products }) {
   )
 }
 
-function CartFooter({className }) {
+function CartFooter ({ className }) {
   const price = 7.96;
   return (
     <div className={`${className} cart-footer`}>
