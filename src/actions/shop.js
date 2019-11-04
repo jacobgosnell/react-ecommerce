4f53b3ac34891aa4fd75b10ba654e233d4dc0b56
Jake <gosnelljacob@gmail.com>
2019-11-04 13:00:57 -0800

    built review product component

--- src/actions/shop.js
+++ src/actions/shop.js
@@ -66,49 +66,56 @@ export function fetchShopProducts() {
         title: 'JavaScript in the Browser',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         price: 1.09,
-        belongsTo: [0, 1]
+        belongsTo: [0, 1],
+        imageUrl: 'http://via.placeholder.com/80x80'
       },
       {
         _id: 1,
         title: 'Graph Database',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         price: 1.09,
-        belongsTo: [0, 6]
+        belongsTo: [0, 6],
+        imageUrl: 'http://via.placeholder.com/80x80'
       },
       {
         _id: 2,
         title: 'Full Stack Development',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         price: 1.09,
-        belongsTo: [0, 1, 4]
+        belongsTo: [0, 1, 4],
+        imageUrl: 'http://via.placeholder.com/80x80'
       },
       {
         _id: 3,
         title: 'UI Design',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         price: 1.09,
-        belongsTo: [0, 2]
+        belongsTo: [0, 2],
+        imageUrl: 'http://via.placeholder.com/80x80'
       },
       {
         _id: 4,
         title: 'JavaScript Development',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         price: 1.09,
-        belongsTo: [0, 1]
+        belongsTo: [0, 1],
+        imageUrl: 'http://via.placeholder.com/80x80'
       },
       {
         _id: 5,
         title: 'UX Design',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         price: 1.09,
-        belongsTo: [0, 2]
+        belongsTo: [0, 2],
+        imageUrl: 'http://via.placeholder.com/80x80'
       },
       {
         _id: 6,
         title: 'OOP Programming',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         price: 1.09,
-        belongsTo: [0, 6]
+        belongsTo: [0, 6],
+        imageUrl: 'http://via.placeholder.com/80x80'
       }
     ]
   })
--- src/components/order/reviewProducts.js
+++ src/components/order/reviewProducts.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
+import ReviewProduct from './reviewProduct';
 
 class ReviewProducts extends Component {
   render() {
@@ -9,7 +10,9 @@ class ReviewProducts extends Component {
       <div className={`${className} review-products`}>
         {
           this.props.cartProducts.map(cartProduct => {
-            return <h1 key={cartProduct._id}>{cartProduct.product.title}</h1>
+            return <ReviewProduct key={cartProduct._id}
+              {...cartProduct}
+            />
           })
         }
       </div>
