8a738302f87f4fd4de9f0b2889d1040994fbfd44
Jake <gosnelljacob@gmail.com>
2019-11-08 22:03:10 -0800

    built the information routes and container components payment and shipping js

---
+++ src/components/information/payment.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+import PageTitle from '../pageTitle';
+
+// REDUX 
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
+
+class Shipping extends Component {
+
+  componentDidMount() {
+    this.props.setHeaderLinks([]);
+    this.props.setNavbarLinks([]);
+  }
+
+  onSubmit = (fields) => {
+    console.log(fields);
+  }
+  
+  render() {
+    return (
+      <div className='sign-in'>
+        <PageTitle className='sign-in__page-title' title='Payment Information' /> 
+        {/*<ShippingForm onSubmit={this.onSubmit} className='sign-in__form'/>*/}
+      </div>
+    );
+  }
+}
+
+Shipping = connect(null, actions)(Shipping)
+
+export default Shipping;
--- src/components/order/reviewForm.js
+++ src/components/order/reviewForm.js
@@ -21,7 +21,7 @@ class ReviewForm extends Component {
         <ReviewProducts className='review-form__products'/>
         <div className='review-form__line'></div>
         <Field className='review-form__proceed'
-        onClick={() => history.push('/signin')}
+        onClick={() => history.push('/information/address')}
         type='submit'
         title='Proceed to Checkout'
         name='proceed'
